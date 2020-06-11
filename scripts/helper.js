82681137f5fa3c44893b4428611e1fe314cb51d9
Ushio <jylzs369@163.com>
2020-06-11 15:25:31 +0800

    fix: site title bug

--- scripts/helper.js
+++ scripts/helper.js
@@ -3,16 +3,19 @@
  * @description Generate page title.
  * @example <%- page_title() %>
  */
-hexo.extend.helper.register("page_title", function () {
-  var title = this.page.title ? this.page.title : (this.config.title || this.theme.page_title)
-
-  if (this.is_archive()) {
-    title += ' ' + this.__('menu.archives')
+hexo.extend.helper.register('page_title', function () {
+  var siteTitle = (this.config.title || this.theme.page_title) + ' '
+  var title = ''
+  if (this.is_home()) {
+    title = siteTitle
+  } else if (this.is_archive()) {
+    title = siteTitle + this.__('menu.archives')
   } else if (this.is_category()) {
-    title += ' ' + this.__('menu.categories')
+    title = siteTitle + this.__('menu.categories') + '：' + this.page.category
   } else if (this.is_tag()) {
-    title += this.__('menu.tags')
+    title = siteTitle + this.__('menu.tags') + '：' + this.page.tag
+  } else {
+    title = siteTitle + this.page.title || ''
   }
-
   return title
 })
